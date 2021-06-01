1559087c0f1cd4d0532d4b633277e43d8f8718ab
Dimas Sudjito <almuhtad@ualberta.ca>
2021-05-31 22:17:15 -0600

    implement upvote and downvote answer

--- graphql/resolvers/answers.js
+++ graphql/resolvers/answers.js
@@ -35,6 +35,9 @@ module.exports = {
       const question = await Question.findById(questionId)
       if (question) {
         const answerIndex = question.answers.findIndex((a) => a.id === answerId)
+        if (answerIndex === -1) {
+          throw new UserInputError('Answer not found')
+        }
         if (question.answers[answerIndex].email === user.email) {
           question.answers.splice(answerIndex, 1)
           await question.save()
@@ -43,7 +46,79 @@ module.exports = {
           throw new AuthenticationError('Action not allowed')
         }
       } else {
-        throw new UserInputError('Answer not found')
+        throw new UserInputError('Question not found')
+      }
+    },
+    upvoteAnswer: async (_, { questionId, answerId }, context) => {
+      const user = auth(context)
+
+      const question = await Question.findById(questionId)
+      if (question) {
+        const answerIndex = question.answers.findIndex((a) => a.id === answerId)
+        if (answerIndex === -1) {
+          throw new UserInputError('Answer not found')
+        }
+        const answer = question.answers[answerIndex]
+        if (answer.upvotes.find((upvote) => upvote.email === user.email)) {
+          // already upvoted, hence un-upvote
+          answer.upvotes = question.answers[answerIndex].upvotes.filter(
+            (upvote) => upvote.email !== user.email
+          )
+        } else {
+          // not yet upvoted, hence upvote
+          answer.upvotes.push({
+            email: user.email,
+            createdAt: new Date().toISOString()
+          })
+          // also un-downvote if downvoted
+          if (
+            answer.downvotes.find((downvote) => downvote.email === user.email)
+          ) {
+            answer.downvotes = answer.downvotes.filter(
+              (downvote) => downvote.email !== user.email
+            )
+          }
+        }
+        await question.save()
+        return question
+      } else {
+        throw new UserInputError('Question not found')
+      }
+    },
+    downvoteAnswer: async (_, { questionId, answerId }, context) => {
+      const user = auth(context)
+
+      const question = await Question.findById(questionId)
+      if (question) {
+        const answerIndex = question.answers.findIndex((a) => a.id === answerId)
+        if (answerIndex === -1) {
+          throw new UserInputError('Answer not found')
+        }
+        const answer = question.answers[answerIndex]
+        if (
+          answer.downvotes.find((downvote) => downvote.email === user.email)
+        ) {
+          // already downvoted, hence un-downvote
+          answer.downvotes = question.answers[answerIndex].downvotes.filter(
+            (downvote) => downvote.email !== user.email
+          )
+        } else {
+          // not yet downvoted, hence downvote
+          answer.downvotes.push({
+            email: user.email,
+            createdAt: new Date().toISOString()
+          })
+          // also un-upvote if upvoted
+          if (answer.upvotes.find((upvote) => upvote.email === user.email)) {
+            answer.upvotes = answer.upvotes.filter(
+              (upvote) => upvote.email !== user.email
+            )
+          }
+        }
+        await question.save()
+        return question
+      } else {
+        throw new UserInputError('Question not found')
       }
     }
   }
