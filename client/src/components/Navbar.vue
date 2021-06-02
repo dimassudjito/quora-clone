cd3c441d73402045517747ea43bcd00af5d97ab7
Dimas Sudjito <almuhtad@ualberta.ca>
2021-06-01 21:57:39 -0600

    set up the visual of the website

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app-bar app color="white" flat dense>
+    <v-img class="ml-16" src="@/assets/logo.png" max-width="75"></v-img>
+    <v-tabs class="ml-8" color="primary">
+      <v-tab v-for="tab in tabs" :key="tab.name" :to="tab.route">
+        <v-icon>{{ tab.icon }}</v-icon>
+      </v-tab>
+    </v-tabs>
+    <v-text-field
+      class="mt-6 mr-4"
+      prepend-inner-icon="mdi-magnify"
+      placeholder="Search Quora"
+      outlined
+      dense
+    ></v-text-field>
+
+    <v-avatar class="mr-16" color="primary" size="25"></v-avatar>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    tabs: [
+      { name: 'Home', icon: 'mdi-home-outline', route: '/' },
+      { name: 'Following', icon: 'mdi-newspaper-variant', route: '/following' },
+      { name: 'Answer', icon: 'mdi-pencil-box-outline', route: '/answer' },
+      { name: 'Spaces', icon: 'mdi-account-group-outline', route: '/spaces' },
+      { name: 'Notification', icon: 'mdi-bell-outline', route: '/notification' }
+    ]
+  })
+}
+</script>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,9 +1,27 @@
 <template>
-  <div>
-    <div v-for="question in getQuestions" :key="question.body">
-      {{ question.body }}
-    </div>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col cols="12" sm="2">
+        <v-sheet rounded="lg" min-height="268">
+          <!--  -->
+        </v-sheet>
+      </v-col>
+
+      <v-col cols="12" sm="8">
+        <v-sheet min-height="70vh" rounded="lg">
+          <div v-for="question in getQuestions" :key="question.body">
+            {{ question.body }}
+          </div>
+        </v-sheet>
+      </v-col>
+
+      <v-col cols="12" sm="2">
+        <v-sheet rounded="lg" min-height="268">
+          <!--  -->
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
