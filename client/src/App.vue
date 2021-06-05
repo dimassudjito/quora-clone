fe9fdef8d25e51e4b63c107a03b716d1fbaf3286
Dimas Sudjito <almuhtad@ualberta.ca>
2021-06-04 20:47:45 -0600

    add login functionality to vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <v-app id="inspire">
-    <Navbar />
-    <v-main class="grey lighten-3">
-      <v-main>
-        <router-view />
-      </v-main>
+    <v-main class="grey lighten-3 pa-16">
+      <Login />
+      <Navbar />
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
 import Navbar from '@/components/Navbar'
+import Login from '@/components/Login'
 
 export default {
-  components: { Navbar }
+  components: { Navbar, Login }
 }
 </script>
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import gql from 'graphql-tag'
+
+import apollo from '@/vue-apollo'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    user: null
   },
   mutations: {
+    SET_USER(state, data) {
+      state.user = data
+      localStorage.setItem('user', JSON.stringify(data))
+    }
   },
-  actions: {
-  },
-  modules: {
-  }
+  actions: {},
+  modules: {}
 })
