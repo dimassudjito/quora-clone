0710e47d056f1625ff5901f59712ee2108d5ce82
Dimas Sudjito <almuhtad@ualberta.ca>
2021-06-07 21:42:47 -0600

    configure project for deployment:

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const { MONGODB } = require('./config.js')
 const typeDefs = require('./graphql/typeDefs')
 const resolvers = require('./graphql/resolvers')
 
+const PORT = process.env.port || 5555
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -15,8 +17,11 @@ mongoose
   .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('MongoDB: quora-clone database connected')
-    return server.listen({ port: 5555 })
+    return server.listen({ port: PORT })
   })
   .then((res) => {
     console.log(`Server is running at ${res.url}`)
+  })
+  .catch((err) => {
+    console.error(err)
   })
